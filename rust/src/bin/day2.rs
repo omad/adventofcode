3a849042a7d295d46f8e3596a61d74bb2cf65339
Damien Ayers <damien@omad.net>
2020-12-03 10:09:48 +1100

    Not working yet Day2P2

--- rust/src/bin/day2.rs
+++ rust/src/bin/day2.rs
@@ -4,28 +4,38 @@ fn main() {
 
     let content = std::fs::read_to_string("day2.input").expect("Couldn't read file");
 
-    let mut valid_passwds: u32 = 0;
+    let mut valid_passwds = 0;
+
+    let mut valid_passwds_pt2 = 0;
     for line in content.lines() {
 //        let foo = line.split(' ').collect();
 
         let v = line.split_whitespace().take(3).collect::<Vec<&str>>();
-        if let [pair, char, passwd] = &v[..] {
-            let bounds: Vec<u32> = pair.split('-').take(2).map(|num| num.parse().unwrap()).collect();
-            let char = char.chars().nth(0).unwrap();
-            let count = count_chars(char, passwd);
+        if let [pair, thechar, passwd] = &v[..] {
+            let bounds: Vec<usize> = pair.split('-').take(2).map(|num| num.parse().unwrap()).collect();
+            let thechar = thechar.chars().nth(0).unwrap();
+            let count = count_chars(thechar, passwd);
             if bounds[0] <= count && count <= bounds[1] {
                 valid_passwds += 1;
             }
             // println!("{}:{}:{}:{}:{}", bounds[0], bounds[1], char, passwd, count);
+
+            if is_valid_pt2(bounds[0], bounds[1], thechar, passwd) {
+                valid_passwds_pt2 += 1;
+            }
         }
 
     }
     println!("{}", valid_passwds);
+    println!("{}", valid_passwds_pt2);
 }
 
+fn is_valid_pt2(pos1: usize, pos2: usize, thechar: char, thestr: &str) -> bool {
+    (thestr.chars().nth(pos1 + 1) == Some(thechar)) ^ (thestr.chars().nth(pos2 + 1) == Some(thechar))
+}
 
-fn count_chars(lookfor: char, mystr: &str) -> u32 {
-    let mut count: u32 = 0;
+fn count_chars(lookfor: char, mystr: &str) -> usize {
+    let mut count = 0;
     for c in mystr.chars() {
         if lookfor == c {
             count += 1;
