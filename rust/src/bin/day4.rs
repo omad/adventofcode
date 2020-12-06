1f6cd7cea8a66e08e8cf0a9378d235e9ce91538e
Damien Ayers <damien@omad.net>
2020-12-06 21:46:56 +1100

    day 4 inco

---
+++ rust/src/bin/day4.rs
@@ -0,0 +1,71 @@
+use std::collections::HashSet;
+
+fn main() {
+
+    let content = std::fs::read_to_string("day4.input").expect("Couldn't read file");
+
+    // let required: HashSet<&'static str> =
+        // ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"].iter().cloned().collect();
+    let required: HashSet<String> =
+        ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"].iter().map(|&s| String::from(s)).collect();
+    let valid_eyes: HashSet<&'static str> =
+        ["amb","blu","brn","gry","grn","hzl","oth"].iter().cloned().collect();
+
+    let mut valid_passports = 0;
+    let mut valid_passports_pt2 = 0;
+    let mut total_lines = 0;
+    let mut total_passports = 0;
+
+    let mut curr_passport: HashSet<String> = HashSet::new();
+    let mut curr_passport_p2: HashSet<String> = HashSet::new();
+    // let mut curr_passport = HashSet::new();
+    for line in content.lines() {
+//        let foo = line.split(' ').collect();
+
+        total_lines += 1;
+        let fields = line.split_whitespace().collect::<Vec<&str>>();
+
+        if fields.len() == 0 {
+            // Check the passport validity
+            if required.is_subset(&curr_passport) {
+                valid_passports += 1;
+            }
+            if required.is_subset(&curr_passport_p2) {
+                valid_passports_pt2 += 1;
+            }
+
+            total_passports +=1;
+            // Start again
+            curr_passport = HashSet::new();
+            continue;
+        } else {
+            // Keep collecting fields
+            for field in fields {
+                let field_name = field.chars().take(3).collect::<String>();
+
+                let field_val = field.chars().skip(4).collect::<String>();
+                let valid = match field_name.as_str() {
+                    "byr" => {
+                        let year: i32 = field_val.parse().unwrap();
+                        1920 <= year && year <= 2002
+                    },
+                    "iyr" => {
+                        let year: i32 = field_val.parse().unwrap();
+                        2010 <= year && year <= 2020
+                    },
+                    _ => false
+
+                };
+                curr_passport.insert(field_name.clone());
+                if valid {
+                    curr_passport_p2.insert(field_name);
+                }
+            }
+        }
+    }
+    if required.is_subset(&curr_passport) {
+        valid_passports += 1;
+    }
+    println!("Lines: {} Passports: {} Valid: {}", total_lines, total_passports, valid_passports);
+    println!("{}", valid_passports_pt2);
+}
