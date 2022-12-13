9a13b2202f76eebdaa955eaa62370862a101f073
Damien Ayers <damien@omad.net>
2022-12-13 17:23:49 +1100

    2022/day9/p2

--- 2022/day9.py
+++ 2022/day9.py
@@ -23,23 +23,18 @@ class Position:
         ox, oy = other
         return self.x == ox and self.y == oy
 
+MOVES = {
+    'R': (1, 0),
+    'L': (-1, 0),
+    'U': (0, 1),
+    'D': (0, -1)
+}
 
-if __name__ == "__main__":
-    with open(sys.argv[1]) as f:
-        lines = f.readlines()
-
+def part_1(lines):
     H = Position(0, 0)
     T = Position(0, 0)
 
-    MOVES = {
-        'R': (1, 0),
-        'L': (-1, 0),
-        'U': (0, 1),
-        'D': (0, -1)
-    }
-
-    tail_locs = set()
-    tail_locs.add(T)
+    tail_locs = set([T])
 
     for line in lines:
         direction, steps = line.strip().split()
@@ -74,3 +69,51 @@ if __name__ == "__main__":
             tail_locs.add(T)
 
     print(len(tail_locs))
+
+
+def part_2(lines):
+    rope = [Position(0,0) for _ in range(10)]
+    tail_locs = set([rope[-1]])
+
+    for line in lines:
+        direction, steps = line.strip().split()
+        print(line.strip())
+
+        for _ in range(int(steps)):
+            rope[0] = rope[0] + MOVES[direction]
+
+            for n in range(1, 10):
+                # Check and move tail
+                H = rope[n-1]
+                T = rope[n]
+                if H.is_touching(T):
+                    continue
+                else:
+                    if H.x == T.x:
+                        if H.y > T.y:
+                            rope[n] = T + (0, 1)
+                        else:
+                            rope[n] = T + (0, -1)
+                    elif H.y == T.y:
+                        if H.x > T.x:
+                            rope[n] = T + (1, 0)
+                        else:
+                            rope[n] = T + (-1, 0)
+                    else: # diagonal
+                        rope[n] = Position(
+                                T.x + 1 * (1 if T.x < H.x else -1),
+                                T.y + 1 * (1 if T.y < H.y else -1)
+                                )
+
+            tail_locs.add(rope[-1])
+
+    print(len(tail_locs))
+
+
+if __name__ == "__main__":
+    with open(sys.argv[1]) as f:
+        lines = f.readlines()
+
+    part_1(lines)
+    part_2(lines)
+
